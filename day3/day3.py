de051348ab6c05ba858ad4f3e2e1207dcda97fa4
nathan <njgingrich@gmail.com>
2015-12-06 21:51:13 -0500

    solution for day 3 in python

--- day3/day3.py
+++ day3/day3.py
@@ -4,10 +4,8 @@ class Position:
 
 max = Position()
 directions = []
-santa = Position()
 
 with open('input.txt', 'r') as f:
-    santa = Position()
     while True:
         c = f.read(1)
         if not c:
@@ -15,31 +13,37 @@ with open('input.txt', 'r') as f:
         directions.append(c)
         if c == '^':
             max.y += 1
-            santa.y += 1
-        elif c == 'v':
-            santa.y -= 1
         elif c == '>':
             max.x += 1
-            santa.x += 1
-        elif c == '<':
-            santa.x -= 1
-        else:
-            print("max X: %d, max y: %d" % (max.x, max.y))
-            print("santa is at (%d, %d)" % (santa.x, santa.y))
-            break
 
 map = [[0 for x in range(max.x)] for x in range(max.y)]
-checker = Position()
+
+santa = Position()
+roboSanta = Position()
+
+turn = 0
 for d in directions:
-    if d == '^':
-        checker.y += 1
-    elif d == 'v':
-        checker.y -= 1
-    elif d == '>':
-        checker.x += 1
-    elif d == '<':
-        checker.x -= 1
-    map[checker.x][checker.y] += 1
+    if turn % 2 == 0:
+        if d == '^':
+            santa.y += 1
+        elif d == 'v':
+            santa.y -= 1
+        elif d == '>':
+            santa.x += 1
+        elif d == '<':
+            santa.x -= 1
+        map[santa.x][santa.y] += 1
+    else: 
+        if d == '^':
+            roboSanta.y += 1
+        elif d == 'v':
+            roboSanta.y -= 1
+        elif d == '>':
+            roboSanta.x += 1
+        elif d == '<':
+            roboSanta.x -= 1
+        map[roboSanta.x][roboSanta.y] += 1
+    turn += 1
 
 houses = 0
 for y in range(max.y):
