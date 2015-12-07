e57a31efe0ff7a9752f81f7a5332d6e9a4f2f958
nathan <njgingrich@gmail.com>
2015-12-06 20:54:51 -0500

    solution for day 2 in python

--- day2/day2.py
+++ day2/day2.py
@@ -4,6 +4,11 @@ def totalNeeded(l, w, h):
 def surfaceArea(l, w, h):
     return 2*(w*l + w*h + h*l)
 
+def ribbonLength(l, w, h):
+    numbers = [l, w, h]
+    numbers.sort()
+    return l*w*h + 2*(numbers[0] + numbers[1])
+
 def minArea(l, w, h):
     numbers = [l, w, h]
     numbers.sort()
@@ -11,11 +16,13 @@ def minArea(l, w, h):
 
 with open('input.txt', 'r') as f:
     totalFeet = 0
+    totalLength = 0
     while True:
         line = f.readline().rstrip()
         if not line:
-            print totalFeet
+            print("total feet:\t %d" % totalFeet)
+            print("ribbon length:\t %d" % totalLength)
             break
         dims = line.split('x')
         totalFeet += totalNeeded(int(dims[0]), int(dims[1]), int(dims[2]))
-
+        totalLength += ribbonLength(int(dims[0]), int(dims[1]), int(dims[2]))
