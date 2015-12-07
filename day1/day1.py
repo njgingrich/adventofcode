9d547b7c5a0b5bbcaafb98d94cf687be28e926de
nathan <njgingrich@gmail.com>
2015-12-06 20:23:03 -0500

    solved for python day 1

---
+++ day1/day1.py
@@ -0,0 +1,18 @@
+count = 0
+position = 0
+with open('input.txt', 'r') as f:
+    while True:
+        c = f.read(1)
+        position += 1
+        if not c:
+            print(count)
+            break
+        if count == -1:
+            print(position)
+            break
+        if c == "(":
+            count += 1
+        elif c == ")":
+            count -= 1
+        else:
+            continue
