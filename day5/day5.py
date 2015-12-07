6ed2baadf09a3a8992f30c7eb14f93e4d47a6a56
nathan <njgingrich@gmail.com>
2015-12-06 22:43:53 -0500

    solution to day 5 in python

--- day5/day5.py
+++ day5/day5.py
@@ -3,11 +3,13 @@ import re
 vowels  = re.compile("(.*[aeiou].*){3,}")
 doubles = re.compile("([a-z])\\1")
 banned  = re.compile("ab|cd|pq|xy")
+pairs   = re.compile("([a-z]{2}).*\\1")
+mirror  = re.compile("([a-z]).\\1")
 
 with open('input.txt', 'r') as f:
     count = 0
     for line in f:
-        if vowels.search(line) and doubles.search(line) and not banned.search(line):
+        if pairs.search(line) and mirror.search(line):
             count += 1
 
     print("final count: %d" % count)
