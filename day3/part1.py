37afc335e6f2d0e0cf975558b8dc3c98ff4fd00f
nathan <njgingrich@gmail.com>
2015-12-07 22:19:10 -0500

    split day 3 into parts

---
+++ day3/part1.py
@@ -0,0 +1,55 @@
+class Position:
+    x = 0
+    y = 0
+
+max = Position()
+directions = []
+
+with open('input.txt', 'r') as f:
+    while True:
+        c = f.read(1)
+        if not c:
+            break
+        directions.append(c)
+        if c == '^':
+            max.y += 1
+        elif c == '>':
+            max.x += 1
+
+map = [[0 for x in range(max.x)] for x in range(max.y)]
+
+santa = Position()
+
+for d in directions:
+    if d == '^':
+        santa.y += 1
+    elif d == 'v':
+        santa.y -= 1
+    elif d == '>':
+        santa.x += 1
+    elif d == '<':
+        santa.x -= 1
+    map[santa.x][santa.y] += 1
+
+houses = 0
+for y in range(max.y):
+    for x in range(max.x):
+        if map[y][x] > 0:
+            houses += 1
+
+print("houses visited: %d" % houses)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
