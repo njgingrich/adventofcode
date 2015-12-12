e8ebefbac4a12901fa7c085f1171492c06c1f853
nathan <njgingrich@gmail.com>
2015-12-12 13:56:22 -0500

    solution to part 2 of day 9 in python

---
+++ day9/part2.py
@@ -0,0 +1,18 @@
+from collections import defaultdict
+from itertools   import permutations
+
+places = set()
+routes = defaultdict(dict)
+with open('input.txt', 'r') as f:
+    for line in f.readlines():
+        (original, _, new, _, distance) = line.split()
+        places.add(original)
+        places.add(new)
+        routes[original][new] = int(distance)
+        routes[new][original] = int(distance)
+    
+distances = []
+for p in permutations(places):
+    distances.append(sum(map(lambda x, y: routes[x][y], p[:-1], p[1:])))
+
+print(max(distances))
