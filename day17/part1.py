e1845d129864263e30dedde9fc21314b52c0244f
nathan <njgingrich@gmail.com>
2015-12-17 01:52:57 -0500

    solution to part 1 of day 17 in python

---
+++ day17/part1.py
@@ -0,0 +1,17 @@
+import itertools as it
+
+containers = []
+with open('input.txt', 'r') as f:
+    for line in f.readlines():
+        containers.append(int(line.strip()))
+
+combinations = []
+for prod in it.product(range(2), repeat=len(containers)):
+    sum = 0
+    for i,c in enumerate(prod):
+        if c == 1:
+            sum += containers[i] 
+    if sum == 150:
+        combinations.append(prod)
+
+print(len(combinations))
