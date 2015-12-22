6f42efafe6157c7f87050cb44b673da25dceae65
nathan <njgingrich@gmail.com>
2015-12-22 16:43:06 -0500

    solution to part 1 of day 20 in python

---
+++ day20/part1.py
@@ -0,0 +1,18 @@
+def factor(n):
+    result = set()
+    for i in range(1, int(n ** 0.5) + 1):
+        div, mod = divmod(n, i)
+        if mod == 0:
+            result |= {i, div}
+    return result
+
+elf = 0
+input = 36000000
+presents = 0
+while presents < input:
+    factors = factor(elf)
+    presents = sum(map(lambda x: 10*x, factors))
+    #print(str(elf) + ": " + str(presents))
+    elf += 1
+
+print elf-1
