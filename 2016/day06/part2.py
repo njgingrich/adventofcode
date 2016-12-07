64f5eaf618f5c9f5c67c896de9251027d15727a8
Nathan <njgingrich@gmail.com>
2016-12-07 13:31:11 -0500

    added solution to part 2 of day 6

---
+++ 2016/day06/part2.py
@@ -0,0 +1,23 @@
+from collections import Counter
+
+lines = []
+
+with open('input.txt', 'r') as f:
+    for line in f:
+        lines.append(list(line.strip()))
+
+i = 0
+chars = []
+freq = []
+
+while i < len(lines[0]):
+    for item in lines:
+        chars.append(item[i])
+
+    l = Counter(chars)
+    print("appending {}".format(l.most_common(1)[0]))
+    freq.append(l.most_common()[-1][0])
+    chars = []
+    i += 1
+
+print("result: {}".format(''.join(freq)))
