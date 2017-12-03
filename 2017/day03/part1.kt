43fe9f2f5fc28fb68bab55af52799a9108692a26
Nathan Gingrich <njgingrich@users.noreply.github.com>
2017-12-03 03:34:29 -0500

    Add spiral function to part 1

--- 2017/day03/part1.kt
+++ 2017/day03/part1.kt
@@ -5,3 +5,24 @@ fun main(args: Array<String>) {
   val coord = spiral(dim).get(input - 1)
   println(Math.abs(coord.first) + Math.abs(coord.second))
 }
+
+fun spiral(dim: Int): List<Pair<Int, Int>> {
+  var x: Int = 0 
+  var y: Int = 0
+  var dx: Int = 0
+  var dy: Int = -1
+  var coords = mutableListOf<Pair<Int, Int>>()
+
+  for (i in 2..Math.pow(dim.toDouble(), 2.toDouble()).toInt()) {
+    if (x == 0 && y == 0) {
+      coords.add(Pair(x, y))
+    }
+    if (x == y || (x < 0 && x == -y) || (x > 0 && x == 1-y)) {
+      dx = -dy.also { dy = dx }
+    }
+    x += dx
+    y += dy
+    coords.add(Pair(x, y))
+  }
+  return coords
+}
