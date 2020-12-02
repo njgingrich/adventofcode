a4eb75f417c549d4565095efa0150bd9126f8de7
Nathan Gingrich <njgingrich@gmail.com>
2020-12-02 09:07:08 -0600

    Add day 4 to 2015 in ts

---
+++ 2015/day04/part2.ts
@@ -0,0 +1,24 @@
+import { createHash } from "https://deno.land/std@0.79.0/hash/mod.ts";
+
+async function readInput(): Promise<string> {
+  return Deno.readTextFile("./input.txt");
+}
+
+function solve(data: string) {
+  let num = -1;
+  let hashOutput = "";
+
+  while (hashOutput.substring(0, 6) !== "000000") {
+    num += 1;
+    const hash = createHash("md5");
+    hash.update(`${data}${num}`);
+    hashOutput = hash.toString();
+  }
+
+  return num;
+}
+
+const data = await readInput();
+console.log(solve(data));
+
+export {};
