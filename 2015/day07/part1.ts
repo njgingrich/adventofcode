03eca02378e5df5ad5a488628cf90f927df3d160
Nathan Gingrich <njgingrich@gmail.com>
2020-12-02 16:24:11 -0600

    2015 day 7 in ts

---
+++ 2015/day07/part1.ts
@@ -0,0 +1,61 @@
+async function readInput(): Promise<string[]> {
+  const file = await Deno.readTextFile("./input.txt");
+  return file.split("\n").filter(Boolean);
+}
+
+const graph: Record<string, string[]> = {};
+const output: Record<string, number> = {};
+
+type TypeOp = "NOT" | "LSHIFT" | "RSHIFT" | "AND" | "OR";
+// deno-lint-ignore no-explicit-any
+const Operations: Record<TypeOp, any> = {
+    'NOT'(x: number) {
+        return ~x & 0xffff;
+    },
+    'LSHIFT'(x: number, y: number) {
+        return x << y;
+    },
+    'RSHIFT'(x: number, y: number) {
+        return x >> y;
+    },
+    'AND'(x: number, y: number) {
+        return x & y;
+    },
+    'OR'(x: number, y: number) {
+        return x | y;
+    },
+}
+
+function read(wire: string) {
+    if (!isNaN(Number(wire))) return Number(wire);
+
+    if (!output[wire]) {
+        let result;
+        const ops = graph[wire];
+
+        if (ops.length === 1) {
+            result = read(ops[0])
+        } else if (ops.length === 2) {
+            result = Operations[ops[0] as TypeOp](read(ops[1]))
+        } else if (ops.length === 3) {
+            result = Operations[ops[1] as TypeOp](read(ops[0]), read(ops[2]));
+        }
+
+        output[wire] = result;
+    }
+    return output[wire];
+}
+
+function solve(lines: string[]) {
+  lines.forEach((line) => {
+    const [operand, result] = line.split(' -> ');
+    graph[result] = operand.split(' ');
+  });
+
+  console.log(read('a'));
+}
+
+const lines = await readInput();
+console.log(solve(lines));
+
+export {};
