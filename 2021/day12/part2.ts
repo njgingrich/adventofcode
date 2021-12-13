1c3f93e91ead8f9e8d962d2b28751d3c51b0de2b
Nathan Gingrich <njgingrich@gmail.com>
2021-12-13 11:58:39 -0600

    2021 day 12 in typescript

---
+++ 2021/day12/part2.ts
@@ -0,0 +1,78 @@
+import * as _ from "lodash";
+import * as path from "path";
+
+import { readInputAsStrings } from "../util";
+
+type AdjacencyList = Record<string, string[]>;
+
+const INPUT_PATH = path.join(__dirname, "./input.txt");
+
+const IsUpper = new RegExp(/[A-Z]/);
+const IsLower = new RegExp(/[a-z]/);
+
+function parse(lines: string[]) {
+  const adjacencyList: AdjacencyList = {};
+
+  for (let line of lines) {
+    const [a, b] = line.split("-");
+    if (Array.isArray(adjacencyList[a])) {
+      adjacencyList[a].push(b);
+    } else {
+      adjacencyList[a] = [b];
+    }
+
+    if (Array.isArray(adjacencyList[b])) {
+      adjacencyList[b].push(a);
+    } else {
+      adjacencyList[b] = [a];
+    }
+  }
+
+  return adjacencyList;
+}
+
+function pathfind(nodes: AdjacencyList, cave: string, history: string[] = []) {
+  let results: string[][] = [];
+  let newHistory = history.concat(cave);
+
+  if (cave === "end") {
+    return [newHistory];
+  }
+
+  for (let neighbor of nodes[cave]) {
+    if (neighbor !== "start") {
+      // We can revisit big caves whenever
+      if (IsUpper.test(neighbor)) {
+        // console.log(`Visiting cave ${neighbor}, from node ${cave}.`);
+        let newResults = pathfind(nodes, neighbor, newHistory);
+        results = results.concat(newResults);
+
+      } else {
+        const caveCounts = _.mapValues(_.groupBy(newHistory.filter(c => IsLower.test(c))), "length");
+        const hasATwoVisit = _.values(caveCounts).some(val => val > 1);
+
+        const count = caveCounts[neighbor] ?? 0;
+
+        if ((hasATwoVisit && count < 1) || (!hasATwoVisit && count < 2)) {
+          // console.log(`Visiting cave ${neighbor}, from node ${cave}.`);
+          let newResults = pathfind(nodes, neighbor, newHistory);
+          results = results.concat(newResults);
+        }
+      }
+    }
+  }
+
+  return results;
+}
+
+function solve(lines: string[]) {
+  const nodes = parse(lines);
+  const results = pathfind(nodes, "start");
+
+  return results.length;
+}
+
+export default async function run() {
+  const input = await readInputAsStrings(INPUT_PATH);
+  return solve(input);
+}
