f5911d52c06ed73b36be47bcdea822bccda71642
Nathan Gingrich <njgingrich@gmail.com>
2023-12-01 08:58:12 -0600

    Day 1 part 1

--- 2023/day01/part1.ts
+++ 2023/day01/part1.ts
@@ -5,13 +5,31 @@ import { readInputAsStrings } from "../util";
 
 const INPUT_PATH = path.join(__dirname, "./input.txt");
 
-function parse(lines: string[]) {
-    return lines;
+function parse(lines: string[]): number[] {
+    // Take input like 1abc2 and return a 2-digit number created from the 1st and last digits
+    return lines.map(line => {
+        let digits: (string | undefined)[] = [undefined, undefined];
+        for (let i = 0; i < line.length; i++) {
+            if (Number.parseInt(line[i], 10)) {
+                digits[0] = line[i];
+                break;
+            }
+        }
+
+        for (let i = line.length - 1; i >= 0; i--) {
+            if (Number.parseInt(line[i], 10)) {
+                digits[1] = line[i];
+                break;
+            }
+        }
+
+        return Number.parseInt(digits.join(''));
+    });
 }
 
 function solve(lines: string[]) {
     const parsed = parse(lines);
-    return parsed;
+    return it.sum(parsed);
 }
 
 export default async function run() {
