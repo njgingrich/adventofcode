4c29d7313ee9cb896676225c1d4e214b1f8a45a6
Nathan Gingrich <njgingrich@gmail.com>
2023-12-01 08:43:39 -0600

    Start 2023

---
+++ 2023/day01/part1.ts
@@ -0,0 +1,20 @@
+import * as it from "itertools";
+import * as path from "path";
+
+import { readInputAsStrings } from "../util";
+
+const INPUT_PATH = path.join(__dirname, "./input.txt");
+
+function parse(lines: string[]) {
+    return lines;
+}
+
+function solve(lines: string[]) {
+    const parsed = parse(lines);
+    return parsed;
+}
+
+export default async function run() {
+    const input = await readInputAsStrings(INPUT_PATH);
+    return solve(input);
+}
