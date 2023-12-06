d6fe517aef7b35ff9b6a731da56ec3eb73194b9e
Nathan Gingrich <njgingrich@gmail.com>
2023-12-06 09:00:24 -0600

    2023 day 6 part 2

--- 2023/day06/part2.ts
+++ 2023/day06/part2.ts
@@ -1,1 +1,35 @@
-export {};
+import * as it from "itertools";
+import * as path from "path";
+
+import { readInputAsStrings } from "../util";
+
+const INPUT_PATH = path.join(__dirname, "./input.txt");
+
+type Race = [time: number, distance: number];
+
+function parse(lines: string[]): Race {
+    const nums = lines.map(line => Number.parseInt(line.replaceAll(/\s+/g, '').split(':')[1], 10));
+    return [nums[0], nums[1]];
+}
+
+function getDistance(t: number, speed: number) {
+    return t * speed - speed ** 2;
+}
+
+function solve(lines: string[]) {
+    const race = parse(lines);
+
+    let winning = 0;
+    for (let s = 1; s <= race[0]; s++) {
+        if (getDistance(race[0], s) > race[1]) {
+            winning++;
+        }
+    }
+
+    return winning;
+}
+
+export default async function run() {
+    const input = await readInputAsStrings(INPUT_PATH);
+    return solve(input);
+}
